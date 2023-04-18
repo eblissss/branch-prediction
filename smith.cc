677dc1da319b019f1b907c954dad4ee2d7cec0c0
Zoe Batz <zoebatz@Zoes-MBP-2.lan>
2023-04-18 18:39:33 -0400

    format output

--- smith.cc
+++ smith.cc
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <cmath>
 
 using namespace std;
 
@@ -6,6 +7,8 @@ int* smith(int num_bits, char *tracefile) {
 
     int largest_bit = 1 << (num_bits - 1);
     int counter = largest_bit;
+    int max_count = pow(2, num_bits) - 1;
+
 
     bool actual_taken;
     bool pred_taken;
@@ -23,7 +26,7 @@ int* smith(int num_bits, char *tracefile) {
         actual_taken = (line[7] == 't');
         pred_taken = (counter >= largest_bit);
 
-        if (actual_taken && counter < 7)
+        if (actual_taken && counter < max_count)
             counter++;
         else if (!actual_taken && counter > 0)
             counter--;
@@ -32,8 +35,9 @@ int* smith(int num_bits, char *tracefile) {
         if (actual_taken != pred_taken) mispredictions++;
     }
 
-    int *ret = (int *)malloc(sizeof(int) * 2);
+    int *ret = (int *)malloc(sizeof(int) * 3);
     ret[0] = predictions;
     ret[1] = mispredictions;
+    ret[2] = counter;
     return ret;
 }
